a7cddb958981466441e7d34beddbefc3018ba859
kam3r0nz <kam.hungerford@gmail.com>
2021-04-26 19:27:51 -0500

    Changed products list and add mapStateToProps and connect to container

--- src/components/productsList.js
+++ src/components/productsList.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import Product from './Product'
 
 class ProductsList extends React.Component {
-
-    listProducts = () => {
-        return this.props.products.map(product => 
-            <div key={product.id} className="product-card">
-                <img className="product-image" src={product.image} alt={product.name}/>{product.name}<br/> 
-                Price: ${product.price}<br/> 
-                <button className="add-cart-btn">Add To Cart</button>
-            </div>)
-    }
 
     render() {
         return (
             < >
-                {this.listProducts()}
+                Products List
             </ >
         )
     }
--- src/containers/productsContainer.js
+++ src/containers/productsContainer.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchProducts } from '../actions/productActions'
+import ProductsList from '../components/productsList'
 
 class ProductsContainer extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchProducts()
+    }
+    
     render() {
         return (
             < >
             <h1>Products</h1>
+            <ProductsList products={this.props.products}/>
             </ >
         )
     }
 
 }
 
-export default ProductsContainer
+const mapStateToProps = state => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect(mapStateToProps, { fetchProducts })(ProductsContainer)
