53a48ed0df87204e9be22aae8c69bfe8b8206160
kam3r0nz <kam.hungerford@gmail.com>
2021-04-26 19:36:16 -0500

    Successfully lists products in the products list component

--- src/components/productsList.js
+++ src/components/productsList.js
@@ -2,10 +2,19 @@ import React from 'react'
 
 class ProductsList extends React.Component {
 
+    listProducts = () => {
+        return this.props.products.map(product => 
+            <div key={product.id} className="product-card">
+                <img className="product-image" src={product.image} alt={product.name}/>{product.name}<br/> 
+                Price: ${product.price}<br/> 
+                <button className="add-cart-btn">Add To Cart</button>
+            </div>)
+    }
+
     render() {
         return (
             < >
-                Products List
+                {this.listProducts()}
             </ >
         )
     }
