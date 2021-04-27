5277ebc6040883934ec125e1f41dbeb658fcd758
kam3r0nz <kam.hungerford@gmail.com>
2021-04-26 19:09:53 -0500

    Added products container component and corresponding route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home'
+import ProductsContainer from './containers/productsContainer'
 
 function App() {
   return (
@@ -9,9 +10,10 @@ function App() {
       <Router>
         <NavBar />
         <Route exact path="/" component={Home}/>
+        <Route exact path="/products" component={ProductsContainer}/>
       </Router>
     </div>
   );
 }
 
-export default App;
+export default App
