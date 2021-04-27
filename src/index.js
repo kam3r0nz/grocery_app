d161a4941317684f9bc09668e7567155830e57d6
kam3r0nz <kam.hungerford@gmail.com>
2021-04-26 19:17:49 -0500

    Added methods from redux library to initialize store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import rootReducer from './reducers/rootReducer'
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), composeWithDevTools()))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
