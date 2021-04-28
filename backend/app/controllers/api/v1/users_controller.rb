1816bd6b4c73fc298e4e2f515f43386956f51abc
kam3r0nz <kam.hungerford@gmail.com>
2021-04-28 16:40:11 -0500

    Created create action for user controller

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::UsersController < ApplicationController
         render json: users, include: [:cart]
     end
 
+    def create
+        if User.find_by(name: user_params[:name])
+            user = User.find_by(name: user_params[:name])
+            redirect_ to "api/v1/users/#{user.id}"
+        else
+            user = User.create(user_params)
+            render json: user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
 end
