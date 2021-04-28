547c831e7aa41870e8545f2ed20c0380b53d9ac3
kam3r0nz <kam.hungerford@gmail.com>
2021-04-28 18:26:41 -0500

    Fixed error with cart component not rendering if no user

--- src/components/cartList.js
+++ src/components/cartList.js
@@ -3,9 +3,11 @@ import React from 'react'
 class CartList extends React.Component {
 
     listCart = () => {
-        if (this.props.user.cart === null) {
+        if (this.props.user.carts === null || this.props.user.length === 0) {
             return <p>Your cart is currently empty.</p>
-        } 
+        } else {
+            return <div>{this.props.user.carts.map(cart => cart.products)}</div>
+        }
     }
 
     render() {
