1050052f2d0db972bf99dc73852001e971e080d5
kam3r0nz <kam.hungerford@gmail.com>
2021-04-28 16:21:14 -0500

    Created event handlers for user input form

--- src/components/userInput.js
+++ src/components/userInput.js
@@ -2,18 +2,31 @@ import React from 'react'
 
 class UserInput extends React.Component {
 
+    state = {
+        name: ''
+    }
+
+    handleOnChange = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    handleOnSubmit = (e) => {
+        e.preventDefault()
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleOnSubmit}>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name"/>
+                    <input type="text" onChange={this.handleOnChange} name="name"/>
                     <input type="submit"/>
                 </form>
             </div>
         )
     }
-
 
 }
 
