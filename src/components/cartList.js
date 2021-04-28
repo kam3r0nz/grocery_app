aaeba99d4a80de08f7fdfe3e32ded815970af91e
kam3r0nz <kam.hungerford@gmail.com>
2021-04-28 16:10:47 -0500

    Added text for empty cart

--- src/components/cartList.js
+++ src/components/cartList.js
@@ -3,17 +3,26 @@ import React from 'react'
 class CartList extends React.Component {
 
     listCart = () => {
-        return this.props.cart.map(product => <div key={product.id} className="product-card">
-        <img className="product-image" src={product.image} alt={product.name}/>{product.name}<br/> 
-        Price: ${product.price}<br/> 
-        <button className="add-cart-btn" onClick={() => this.handleOnClick(product)}>Add To Cart</button>
-    </div>)
+        if (this.props.cart.length === 0) {
+            return <p>Your cart is currently empty.</p>
+        } else {
+            return this.props.cart.map(product => <div key={product.id} className="product-card">
+            <img className="product-image" src={product.image} alt={product.name}/>{product.name}<br/> 
+            Price: ${product.price}<br/> 
+            <button className="add-cart-btn" onClick={() => this.handleOnClick(product)}>Remove from Cart</button>
+            </div>)
+        }
     }
+
+    // cartTotal = () => {
+    //     return this.props.cart.concat(product.price)
+    // }
 
     render() {
         return (
             < >
             {this.listCart()}
+            {/* {this.cartTotal()} */}
             </ >
         )
     }
