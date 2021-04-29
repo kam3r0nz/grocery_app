c6006c7ea1585bbd06a121fb5c83f81f763ed4c4
kam3r0nz <kam.hungerford@gmail.com>
2021-04-28 19:01:41 -0500

    Styled rendered product in cart with remove button

--- src/components/cartList.js
+++ src/components/cartList.js
@@ -8,11 +8,20 @@ class CartList extends React.Component {
         }
     }
 
+    listProducts = () => {
+        return this.props.cart.products.map(product => 
+            <div key={product.id} className="product-card">
+                <img className="product-image" src={product.image} alt={product.name}/>{product.name}<br/> 
+                Price: ${product.price}<br/> 
+                <button className="add-cart-btn" onClick={() => this.handleOnClick(product)}>Remove From Cart</button>
+            </div>)
+    }
+
     listCart = () => {
         if (this.props.user.cart === null || this.props.user.length === 0) {
             return <p>Your cart is currently empty.</p>
         } else {
-            return <div>{this.props.cart.products.map(product => product.name)}</div>
+            return <div>{this.listProducts()}</div>
         }
     }
 
