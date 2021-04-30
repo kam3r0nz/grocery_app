5cf2e8055df79910e34cdba7442f8ddaf4dd3f40
kam3r0nz <kam.hungerford@gmail.com>
2021-04-29 22:36:56 -0500

    Removed helper method from products controller

--- backend/app/controllers/api/v1/products_controller.rb
+++ backend/app/controllers/api/v1/products_controller.rb
@@ -1,21 +1,12 @@
 class Api::V1::ProductsController < ApplicationController
     def index
-        if filter_carts
-            cart_products = @cart.products
-            render json: cart_products
-        else
-            products = Product.all
-            render json: products
-        end
+        cart_products = @cart.products
+        render json: cart_products
     end
 
     private
 
     def product_params
         params.require(:product).require(:name, :price, :image)
-    end
-
-    def filter_carts
-        params[:cart_id] && @cart = Cart.find_by_id(params[:cart_id])
     end
 end
