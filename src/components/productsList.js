96f802d9bb095c86a38e70604539a509cb320390
kam3r0nz <kam.hungerford@gmail.com>
2021-05-01 22:03:26 -0500

    Removed view button and product componet

--- src/components/productsList.js
+++ src/components/productsList.js
@@ -5,17 +5,12 @@ class ProductsList extends React.Component {
     handleOnClick = (product) => {
         this.props.addToCart(product)
     }
-    
-    handleOnViewClick = () => {
-        
-    }
 
     listProducts = () => {
         return this.props.products.map(product => 
             <div key={product.id} className="product-card">
                 <img className="product-image" src={product.image} alt={product.name}/><h6>{product.name}</h6>
                 Price: ${product.price}<br/>
-                <button className="cart-btn" onClick={() => this.handleOnViewClick(product)}>View</button>
                 <button className="cart-btn" onClick={() => this.handleOnClick(product)}>Add To Cart</button>
             </div>)
     }
